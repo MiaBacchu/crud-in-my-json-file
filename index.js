8e96e08b8b4df999a89832e042f860320ededb81
Mia Bacchu <jewelranabacchu@gmail.com>
2022-09-17 13:40:01 +0600

    port added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoute = require('./routes/v1/userRoute');
 const app = express();
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use('/user',userRoute);
 app.all('*',(req,res)=>{
     res.send('No Routes Found')
